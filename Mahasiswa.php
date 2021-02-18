bd0d4f0e7548e9cd23db9442bf91d421d9a7b3fd
bayufirmadi <bayufirmadi18@gmail.com>
2021-02-18 20:10:28 +0700

    praktikum 02 membuat objek dan class

---
+++ Mahasiswa.php
@@ -0,0 +1,18 @@
+<?php
+class Mahasiswa 
+{
+   public $nim;
+   public $nama;
+   public $tanggal_lahir;
+   public $jenis_kelamin;
+ 
+   public function tambpilkanAngkatan()
+   {
+       
+   }
+ 
+   public function tampilkanUmur()
+   {
+       
+   }
+}
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+ 
+include('Mahasiswa.php');
+
+$bayu = new Mahasiswa();
+$tegar = new Mahasiswa();
+$andri = new Mahasiswa();
+ 
+$bayu->nim = 1101191047;
+$bayu->nama = bayufirmadi;
+$bayu->tanggal_lahir = 18-10-2001 ;
+$bayu->jenis_kelamin = laki ;
+
+$tegar->nim = 1101191067;
+$tegar->nama = tegarferdigantara;
+$tegar->tanggal_lahir = 09-02-2001 ;
+$tegar->jenis_kelamin = laki ;
+
+$andri->nim = 1101191056;
+$andri->nama = andrimaulana;
+$andri->tanggal_lahir = 13-06-2001 ;
+$andri->jenis_kelamin = laki ;
+
+
+
