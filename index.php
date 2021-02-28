b1f08105be5db89882b3c29179cf492fc907f4bc
bayufirmadi <bayufirmadi18@gmail.com>
2021-02-28 23:45:35 +0700

    praktikum 02 membuat objek dan classpraktikum 02 membuat objek dan class

--- index.php
+++ index.php
@@ -1,25 +1,33 @@
 <?php
- 
-include('Mahasiswa.php');
-
-$bayu = new Mahasiswa();
-$tegar = new Mahasiswa();
-$andri = new Mahasiswa();
- 
-$bayu->nim = 1101191047;
-$bayu->nama = bayufirmadi;
-$bayu->tanggal_lahir = 18-10-2001 ;
-$bayu->jenis_kelamin = laki ;
-
-$tegar->nim = 1101191067;
-$tegar->nama = tegarferdigantara;
-$tegar->tanggal_lahir = 09-02-2001 ;
-$tegar->jenis_kelamin = laki ;
-
-$andri->nim = 1101191056;
-$andri->nama = andrimaulana;
-$andri->tanggal_lahir = 13-06-2001 ;
-$andri->jenis_kelamin = laki ;
+    require_once ("Mahasiswa.php");
+    require_once ("MahasiswaBaru.php");
 
 
+    $bayu = new Mahasiswa('H1101191035', 'Bayu firmadi', '2001-10-18', 'Pria');
+    $tegar = new MahasiswaBaru('H1101191035', 'Tegar ferdigantara', '2002-10-08', 'Pria',1134);
 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Test</title>
+</head>
+<body>
+    <?php echo "<p>Mahasiswa prodi Sistem Infomasi</p>"; ?>
+    
+    <?php
+        $bayu->tampilkanAngkatan();
+        echo "<br>";
+        $tegar->tampilkanAngkatan();
+        echo "<br>";
+        echo "<br>";
+        $bayu->tampilkanUmur();
+        echo "<br>";
+        $tegar->tampilkanUmur();
+    ?>
+</body>
+</html>
