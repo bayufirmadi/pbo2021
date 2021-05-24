fb5d94bc46b8f355eb1eb92ece36d838ffa57195
bayufirmadi <bayufirmadi18@gmail.com>
2021-05-24 22:09:31 +0700

    Relasi pada OOP

--- application/Product.php
+++ application/Product.php
@@ -1,15 +1,34 @@
 <?php
-    namespace App;
 
-    class Product 
+
+namespace App;
+
+class Product 
     {
-        protected $_type = "Book";
+        private $_type = 'Book';
+        private $id;
+        private $name;
+        private $price;
 
-        public function setProductType($type){
+        function __construct($id, $name, $price)
+        {
+            $this->id = $id;
+            $this->name = $name;
+            $this->price = $price;
+        }
+
+        public function changePrice($value)
+        {
+            $this->price = $value;
+        }
+
+        public function setProductType($type)
+        {
             $this->_type = $type;
         }
 
-        public function getProductType(){
+        public function getProductType()
+        {
             return $this->_type;
         }
     }
